c09ada0f70a76461e6e10a2434290c524b58c55c
thrishik7 <thrishik.s@gmail.com>
2020-02-08 05:04:34 +0530

    prof/subj

--- app.py
+++ app.py
@@ -1,32 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 import pyrebase 
 
 config={
-  "apiKey": "AIzaSyCKY7quMaY6AR76MwersdPvUqTLDEu3AKM",
-  "authDomain": "cool-d58c6.firebaseapp.com",
-  "databaseURL": "https://cool-d58c6.firebaseio.com",
-  "projectId": "cool-d58c6",
-  "storageBucket": "cool-d58c6.appspot.com",
-  "messagingSenderId": "333224819338",
-  "appId": "1:333224819338:web:6bd1ea7ed4729f05"
+  "apiKey": "AIzaSyAcCrQDXL4NfjwRbldL7id0TtgvYgkb8gc",
+  "authDomain": "aztech-e3e7f.firebaseapp.com",
+  "databaseURL": "https://aztech-e3e7f.firebaseio.com",
+  "projectId": "aztech-e3e7f",
+  "storageBucket": "aztech-e3e7f.appspot.com",
+  "messagingSenderId": "1056871715099",
+  "appId": "1:1056871715099:web:ac57e407031b02728143be"
 }
 
 firebase = pyrebase.initialize_app(config)
 
 db= firebase.database()
-
-db.child("names").push({"name":"tsk"})
-
-
-
-@app.route("/")
-def home_page():
-    online_users = mongo.db.users.find({"online": True})
-    return online_users
-
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True, port=7000)
 
