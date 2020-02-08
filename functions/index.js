bd1147de0c81e7e29fd11c1808215dfa9ddf1671
Smrithi23 <smrithi.prakash23@gmail.com>
2020-02-08 16:04:04 +0530

    updated

--- functions/index.js
+++ functions/index.js
@@ -2,10 +2,13 @@ const firebase = require("firebase");
 const functions = require("firebase-functions");
 const login = require('./login.js');
 const student= require("./student.js");
+const student_today_attendance = require("student_today_attendance");
+const subjects = require("./subjects.js");
+
 exports.studentLogin = login.studentLogin;
 exports.profLogin = login.profLogin;
 exports.sub_percentage=student.sub_percentage;
 exports.message_to_prof= student.message_to_prof;
-
 exports.getSubjects = subjects.getSubjects;
 exports.getClass = subjects.getClass;
+exports.student_today_attendance = student_today_attendance.attendance_today;
--- functions/student_attendance_today.js
+++ functions/student_attendance_today.js
@@ -3,11 +3,38 @@ const config = require("./config.js");
 const db = config.db;
 
 exports.attendance_today = functions.https.onRequest(async (req, res) => {
-    try {
-        let rollno = req.query.rollno;
-        
-    } catch(err) {
-        console.log(err);
-        res.status(500).send("Internal Server Error");
-    }
+    return jwtCheck(req, res, async() => {
+        try {
+            let rollno = req.query.rollno;
+            let ref = await db.ref(`/Students/${rollno}/subjects`);
+            let snapshot = await ref.once("value");
+            let present = [];
+            let absent = [];
+            let subjects = [];
+            let attendance = {};
+            let date = Date().slice(0, 15);
+            console.log(date);
+            snapshot.forEach(child => {
+                subjects.push(child.key);
+            });
+            console.log(subjects);
+            await Promise.all(
+                subjects.map(async subject => {
+                    console.log(subject);
+                    let child_ref = await db.ref(`/Students/${rollno}/subjects/${subject}/attendance`);
+                    let snap = await child_ref.once("value");
+                  
+    
+                    if(snap.hasChild(`\"${date}\"`)) {
+                           
+                            attendance[`${subject}`] = snap.child(`\"${date}\"`).val();
+                        }
+                    })
+            )
+            res.status(200).send(attendance);
+        } catch(err) {
+            console.log(err);
+            res.status(500).send("Internal Server Error");
+        }
+    })
 })
