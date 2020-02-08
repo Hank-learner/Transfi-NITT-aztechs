1feb6b8130077df793c56074f287c380408c6ad1
quantum-jo <thejojobrothers@gmail.com>
2020-02-08 13:00:54 +0530

    Added subjects file for prof

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,11 @@
 const firebase = require("firebase");
 const functions = require("firebase-functions");
 const login = require('./login.js');
+const subjects = require('./subjects');
 
 exports.studentLogin = login.studentLogin;
 exports.profLogin = login.profLogin;
+
+//working
+exports.getSubjects = subjects.getSubjects;
+exports.getClass = subjects.getClass;
