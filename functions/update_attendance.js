db246f3142170962403688ea128945517e9db0da
quantum-jo <thejojobrothers@gmail.com>
2020-02-09 00:41:19 +0530

    Added feature to create and store image

--- functions/update_attendance.js
+++ functions/update_attendance.js
@@ -6,83 +6,92 @@ const fs = require("fs");
 const cors = require('cors')({
     origin: true
 });
+const base64ToImage = require('base64-to-image');
+const path = "../output/rollno_list.txt";
 
 //function check_student()
 exports.update_attendance = functions.https.onRequest(async (req, res) => {
     // return middleware.jwtCheck(req, res, async () => {
         try {
             return cors(req, res, async () => {
-            let rollno = fs.readFileSync("rollno_list.txt").split(" ");
-            let profid = req.body.profid;
-            let subject = req.body.subject;
-            let date = new Date().toString().slice(0, 15);
-            let absent_list = []
-            let ref1 = await db.ref(`/Professors/${profid}/subjects/${subject}`);
-            let snapshot = await ref1.once("value");
-            let d = 0;
-            snapshot.forEach(childSnapshot => {
-                d = 0;
-                for (var i = 0; i < rollno.length; i++) {
-                    if (childSnapshot.key == rollno[i])
-                        d = 1;
-                }
-                if (d == 0)
-                    absent_list.push(childSnapshot.key);
-            })
-            console.log(absent_list);
-            await Promise.all(
-                absent_list.map(async (rollno) => {
-                    let ref = await db.ref(`/Professors/${profid}/subjects/${subject}/${rollno}`);
-                    let snapshot = await ref.once("value");
-                    let per = snapshot.child(`percentage`).val();
-                    let total = snapshot.child(`total_class`).val();
-                    per *= 0.01;
-                    per = (per * total) / (total + 1);
-                    per *= 100;
-                    total = total + 1;
-                    await ref.update({
-                        [`percentage`]: per,
-                        [`total_classes`]: total
-                    });
-                    let ref2 = db.ref(`/Students/${rollno}/subjects/${subject}/attendance`);
-                    await ref2.update({
-                        [`${date}`]: 0
-                    });
-                    let ref3 = db.ref(`/Students/${rollno}/subjects/${subject}`);
-                    await ref3.update({
-                        percentage: per,
-                        total: total
-                    });
+                if(fs.existsSync(path)) fs.unlinkSync(path);
+                
+                let base64str = req.body.image;
+                let image_path = '../captured/';
+                let optionObj = {'fileName': Date.now().toString(), 'type': 'jpg'};
+                base64ToImage(base64str, image_path, optionObj);
+                while(!fs.existsSync(path));
+                let rollno = fs.readFileSync("rollno_list.txt").split(" ");
+                let profid = req.body.profid;
+                let subject = req.body.subject;
+                let date = new Date().toString().slice(0, 15);
+                let absent_list = []
+                let ref1 = await db.ref(`/Professors/${profid}/subjects/${subject}`);
+                let snapshot = await ref1.once("value");
+                let d = 0;
+                snapshot.forEach(childSnapshot => {
+                    d = 0;
+                    for (var i = 0; i < rollno.length; i++) {
+                        if (childSnapshot.key == rollno[i])
+                            d = 1;
+                    }
+                    if (d == 0)
+                        absent_list.push(childSnapshot.key);
                 })
-            )
+                console.log(absent_list);
+                await Promise.all(
+                    absent_list.map(async (rollno) => {
+                        let ref = await db.ref(`/Professors/${profid}/subjects/${subject}/${rollno}`);
+                        let snapshot = await ref.once("value");
+                        let per = snapshot.child(`percentage`).val();
+                        let total = snapshot.child(`total_class`).val();
+                        per *= 0.01;
+                        per = (per * total) / (total + 1);
+                        per *= 100;
+                        total = total + 1;
+                        await ref.update({
+                            [`percentage`]: per,
+                            [`total_classes`]: total
+                        });
+                        let ref2 = db.ref(`/Students/${rollno}/subjects/${subject}/attendance`);
+                        await ref2.update({
+                            [`${date}`]: 0
+                        });
+                        let ref3 = db.ref(`/Students/${rollno}/subjects/${subject}`);
+                        await ref3.update({
+                            percentage: per,
+                            total: total
+                        });
+                    })
+                )
 
-            await Promise.all(
-                rollno.map(async (item) => {
-                    let ref = await db.ref(`/Professors/${profid}/subjects/${subject}/${item}`);
-                    let snap = await ref.once("value");
-                    let per = snap.child(`percentage`).val();
-                    let total = snap.child(`total_classes`).val();
-                    per *= 0.01;
-                    per = ((per * total) + 1) / (total + 1);
-                    per *= 100;
-                    total = total + 1;
-                    
-                    await ref.update({
-                        [`percentage`]: per,
-                        [`total_classes`]: total
-                    });
-                    let ref2 = db.ref(`/Students/${item}/subjects/${subject}/attendance`);
-                    await ref2.update({
-                        [`${date}`]: 1
-                    });
-                    let ref3 = db.ref(`/Students/${item}/subjects/${subject}`);
-                    await ref3.update({
-                        percentage: per,
-                        total: total
-                    });
-                })
-            )
-            res.status(200).send("Updated");
+                await Promise.all(
+                    rollno.map(async (item) => {
+                        let ref = await db.ref(`/Professors/${profid}/subjects/${subject}/${item}`);
+                        let snap = await ref.once("value");
+                        let per = snap.child(`percentage`).val();
+                        let total = snap.child(`total_classes`).val();
+                        per *= 0.01;
+                        per = ((per * total) + 1) / (total + 1);
+                        per *= 100;
+                        total = total + 1;
+                        
+                        await ref.update({
+                            [`percentage`]: per,
+                            [`total_classes`]: total
+                        });
+                        let ref2 = db.ref(`/Students/${item}/subjects/${subject}/attendance`);
+                        await ref2.update({
+                            [`${date}`]: 1
+                        });
+                        let ref3 = db.ref(`/Students/${item}/subjects/${subject}`);
+                        await ref3.update({
+                            percentage: per,
+                            total: total
+                        });
+                    })
+                )
+                res.status(200).send("Updated");
             });
         } catch (err) {
             console.log(err);
