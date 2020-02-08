8e2c62c95d9ed4165ae4727fed39f86d5509dc16
Vishal Saranathan <s-vishal1999@hotmail.com>
2020-02-08 12:13:01 +0530

    Final face model

--- train_model.py
+++ train_model.py
@@ -11,11 +11,11 @@ import pickle
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-e", "--embeddings", required=True,
-	help="path to serialized db of facial embeddings")
+                help="path to serialized db of facial embeddings")
 ap.add_argument("-r", "--recognizer", required=True,
-	help="path to output model trained to recognize faces")
+                help="path to output model trained to recognize faces")
 ap.add_argument("-l", "--le", required=True,
-	help="path to output label encoder")
+                help="path to output label encoder")
 args = vars(ap.parse_args())
 
 # load the face embeddings
@@ -30,7 +30,7 @@ labels = le.fit_transform(data["names"])
 # train the model used to accept the 128-d embeddings of the face and
 # then produce the actual face recognition
 print("[INFO] training model...")
-recognizer = SVC(C=1.0, kernel="linear", probability=True)
+recognizer = SVC(C=0.7 , kernel="linear", probability=True)
 recognizer.fit(data["embeddings"], labels)
 
 # write the actual face recognition model to disk
